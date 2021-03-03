b7d625423d9ec066d6ebe1de83bea2acfa861194
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-03-03 20:55:40 +0100

    create naive SimpleBankAccountWithAtm

---
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,30 @@
+package lab01.example.model;
+
+/**
+ * This class represent a particular instance of a BankAccount.
+ * It applies a fee on deposit and withdraw.
+ */
+public class SimpleBankAccountWithAtm implements BankAccount {
+    public SimpleBankAccountWithAtm(AccountHolder accountHolder, int i) {
+    }
+
+    @Override
+    public AccountHolder getHolder() {
+        return null;
+    }
+
+    @Override
+    public double getBalance() {
+        return 0;
+    }
+
+    @Override
+    public void deposit(int usrID, double amount) {
+
+    }
+
+    @Override
+    public void withdraw(int usrID, double amount) {
+
+    }
+}
---
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,21 @@
+import lab01.example.model.AccountHolder;
+import lab01.example.model.BankAccount;
+import lab01.example.model.SimpleBankAccountWithAtm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleBankAccountWithAtmTest {
+    private static final int VALID_USER_ID = 1;
+    private AccountHolder accountHolder;
+    private BankAccount bankAccount;
+
+    @BeforeEach
+    void beforeEach(){
+        accountHolder = new AccountHolder("Mario", "Rossi", VALID_USER_ID);
+        bankAccount   = new SimpleBankAccountWithAtm(accountHolder, 0);
+    }
+}
